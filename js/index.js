7c7a5e6453f79c6519d6ad626f01e686029bfd2c
Drew Stokes <drew@drewstokes.com>
2015-03-09 11:50:14 -0700

    limit highlight languages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+// specify relevant languages for highlight
+hljs.configure({
+  languages: [
+    'bash', 'c#', 'c++', 'json', 'html', 'makefile', 'perl', 'python'
+  ]
+});
+
+
 $('pre.programlisting, pre.example').each(function(i, block) {
   hljs.highlightBlock(block);
 });
